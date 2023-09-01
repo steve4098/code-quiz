52fe3723e577e42b539c36cc09f2723d75a161b2
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-01 18:42:49 +0100

    answers added for all questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,26 +3,32 @@
 let questions = [
     {
         "title": "Which of these is not a common data type:",
-        "choices": ["string", "number", "alert", "boolean"]
+        "choices": ["string", "number", "alert", "boolean"],
+        "answer": "boolean"
     },
     {
         "title": "If/else statements are enclosed with which punctuation:",
-        "choices": ["quotes", "curly brackets", "square brackets", "parentheses"]
+        "choices": ["quotes", "curly brackets", "square brackets", "parentheses"],
+        "answer": "curly brackets"
     },
     {
         "title": "In JavaScript, arrays can be used to store ______:",
-        "choices": ["numbers", "strings", "other arrays", "all of the above"]
+        "choices": ["numbers", "strings", "other arrays", "all of the above"],
+        "answer": "all of the above"
     },
     {
         "title":"What does HTML stand for?",
-        "choices": ["Hypertext Meta Language", "Hypertext Markup Language", "Hypertext Multi Links", "Hypertext Machine Loader"]
+        "choices": ["Hypertext Meta Language", "Hypertext Markup Language", "Hypertext Multi Links", "Hypertext Machine Loader"],
+        "answer": "Hypertext Markup Language"
     },
     {
         "title": "String values must be enclosed with _____:",
-        "choices": ["parentheses", "curly brackets", "quotes", "back ticks"]
+        "choices": ["parentheses", "curly brackets", "quotes", "back ticks"],
+        "answer":  "quotes"
     },
     {
         "title": "A very useful tool used during development and debugging for printing content to the debugger is:",
-        "choices": ["JavaScript", "terminal/bash", "for loops", "console.log"]
+        "choices": ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        "answer": "console.log"
     }
 ]
