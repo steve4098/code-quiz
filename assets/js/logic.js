745bca02b6ef31b79c4aa4943157b234a7728eb7
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-01 19:02:12 +0100

    variables created in logic.js for html elements

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,3 +2,12 @@
 let currentQuestionIndex = 0;
 let timer = questions.length * 12;
 let timerID;
+
+//HTML elements
+let questionsElement = document.querySelector('#questions');
+let timerElement = document.querySelector('#time');
+let choicesElement = document.querySelector('#choices');
+let startButton = document.querySelector('#start');
+let submitButton = document.querySelector('#submit');
+let initialElement = document.querySelector('#initials');
+let feedbackElement = document.querySelector('#feedback');
