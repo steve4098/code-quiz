50cac2f252ea2be54309875c63d9f9d559ec4daa
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-01 18:30:05 +0100

    questions variable created and first two questions written within it

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+// let questionChoice = document.querySelector('#questions');
+
+let questions = [
+    {
+        "title": "Which of these is not a common data type:",
+        "choices": ["string", "number", "alert", "boolean"]
+    },
+    {
+        "title": "If/else statements are enclosed with which punctuation:",
+        "choices": ["quotes", "curly brackets", "square brackets", "parentheses"]
+    },
+    
+]
